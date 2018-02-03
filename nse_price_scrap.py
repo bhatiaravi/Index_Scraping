bdc12f3cd2271541ec8d1d1e58726621f68bdb57
eco <bhatiaravijag@gmail.com>
2018-02-03 15:38:42 +0530

    Added Support for NSE SME

--- nse_price_scrap.py
+++ nse_price_scrap.py
@@ -1,6 +1,6 @@
 import BeautifulSoup as bs
 import json, time
-from nse_codes import NSE_CODES_LIST
+from nse_codes import NSE_CODES_LIST, NSE_SME_CODES
 from common import *
 
 outcome_df = pd.DataFrame(columns=COLUMNS)
@@ -8,7 +8,6 @@ outcome_df = pd.DataFrame(columns=COLUMNS)
 def clean(info):
     x = info.replace('\r','').replace('\n','')
     return json.loads(x)['data'][0]
-
 
 def run_scrap_nse():
     # for NSE MAIN
@@ -35,5 +34,25 @@ def run_scrap_nse():
             print repr(e)
             logging.error("Error in NSE: " + repr(e) + " for code " + code)
     # TODO: NSE SME
-    BASE_URL = 'https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol={0}&illiquid=0&smeFlag=0&itpFlag=0'
+    BASE_URL_SME = 'https://www.nseindia.com/emerge/live_market/dynaContent/live_watch/get_quote_SME/GetSMEQuote.jsp?symbol={0}'
+    for code in NSE_SME_CODES:
+        page = get_page(BASE_URL_SME, code)
+        html = bs.BeautifulSoup(page.content)
+        resp = html.find(id='responseDiv')
+        info = resp.contents[0]
+        try:
+            useful_info = clean(info)
+            company_name = useful_info['companyName']
+            last_price = float(useful_info['lastPrice'].replace(',',''))
+            low_52week = float(useful_info['low52'].replace(',',''))
+            high_52week = float(useful_info['high52'].replace(',',''))
+            price_change = float(useful_info['pChange'].replace(',',''))
+            reco = cond_check(last_price, low_52week, high_52week)
+            if reco is not None:
+                # Stock qualifies for being watched
+                outcome_df.loc[outcome_df.shape[0]] = [company_name, last_price, high_52week, low_52week, code, price_change, reco]
+            time.sleep(SLEEP)
+        except Exception, e:
+            print repr(e)
+            logging.error("Error in NSE SME: " + repr(e) + " for code " + code)
     return outcome_df
